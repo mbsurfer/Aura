d816e70f3ed32f00d0a7bff8434d65d0f4cdeef1
mbsurfer <rneldner@gmail.com>
2023-10-27 10:47:02 -0400

    Added player and enemy characters

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,16 @@
+
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
---
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -0,0 +1,5 @@
+
+
+
+#include "Character/AuraEnemy.h"
+
