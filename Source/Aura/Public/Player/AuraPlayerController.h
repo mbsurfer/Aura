cfb5ac6a913f65fafc0f1ea192dcabc0a2987e3c
mbsurfer <rneldner@gmail.com>
2023-10-27 16:41:11 -0400

    Created player controll system using enhanced input

---
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,29 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	AAuraPlayerController();
+
+protected:
+	virtual void BeginPlay();
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<class UInputMappingContext> AuraContext;
+};
+
+
+
