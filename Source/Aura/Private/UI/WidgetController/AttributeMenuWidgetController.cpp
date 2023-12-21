cf579876a08bda5869f13089890620a3073f8598
mbsurfer <rneldner@gmail.com>
2023-12-21 15:37:09 -0500

    added secondary attributes to the attribute set TagsToAttributes array

--- Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -5,8 +5,8 @@
 
 #include "AbilitySystem/AuraAttributeSet.h"
 #include "AbilitySystem/Data/AttributeInfo.h"
-#include "AuraGameplayTags.h"
 
+// Set the inital attribute values to display in the attribute menu
 void UAttributeMenuWidgetController::BroadcastInitialValues()
 {
     UAuraAttributeSet* AS = CastChecked<UAuraAttributeSet>(AttributeSet);
@@ -21,7 +21,20 @@ void UAttributeMenuWidgetController::BroadcastInitialValues()
     }
 }
 
+// Update attribute menu values when attributes change
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 {
-    
+    UAuraAttributeSet* AS = CastChecked<UAuraAttributeSet>(AttributeSet);
+
+    for (auto& Pair : AS->TagsToAttributes)
+    {
+        AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(Pair.Value()).AddLambda(
+            [this, Pair, AS](const FOnAttributeChangeData& Data)
+            {
+                FAuraAttributeInfo Info = AttributeInfo->GetAttributeInfoByTag(Pair.Key);
+                Info.AttributeValue = Pair.Value().GetNumericValue(AS);
+                AttributeInfoDelegate.Broadcast(Info);
+            }
+        );
+    }
 }
