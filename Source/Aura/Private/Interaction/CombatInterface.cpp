c53fc6ae9c823d94af406e7bd5bf37654e44dcac
mbsurfer <rneldner@gmail.com>
2024-01-08 19:54:11 -0500

    add a face target function to the combat interface

--- Source/Aura/Private/Interaction/CombatInterface.cpp
+++ Source/Aura/Private/Interaction/CombatInterface.cpp
@@ -3,7 +3,6 @@
 
 #include "Interaction/CombatInterface.h"
 
-
 int32 ICombatInterface::GetPlayerLevel()
 {
     return 0;
--- Source/Aura/Public/Interaction/CombatInterface.h
+++ Source/Aura/Public/Interaction/CombatInterface.h
@@ -6,7 +6,9 @@
 #include "UObject/Interface.h"
 #include "CombatInterface.generated.h"
 
-UINTERFACE(MinimalAPI)
+class UMotionWarpingComponent;
+
+UINTERFACE(MinimalAPI, BlueprintType)
 class UCombatInterface : public UInterface
 {
 	GENERATED_BODY()
@@ -20,7 +22,9 @@ class AURA_API ICombatInterface
 	GENERATED_BODY()
 
 public:
-
 	virtual int32 GetPlayerLevel();
 	virtual FVector GetCombatSocketLocation();
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	void UpdateFacingTarget(const FVector& Target);
 };
