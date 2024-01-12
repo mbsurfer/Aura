2943eb9b55da3e2ae8e6d7b5b3c839ef6d357579
mbsurfer <rneldner@gmail.com>
2024-01-12 16:06:57 -0500

    added class for character classes (warrior, ranger, elementalist); created curve tables for primary attributes of enemies

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(const ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
